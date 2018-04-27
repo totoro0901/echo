6dbf4b8c58fa8ddcf721ba942c74fe1cdd1caf2c
王卓 <wangzhuo@haomo-studio.com>
2018-04-27 15:06:26 +0800

    配置

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
 
 // 引入css
 import '@/assets/css/reset.css'
@@ -11,10 +12,13 @@ import '@/assets/css/reset.css'
 // 第三方库
 import MuseUI from 'muse-ui'
 import 'muse-ui/dist/muse-ui.css'
+import _ from 'lodash'
 
 Vue.config.productionTip = false
 
 Vue.use(MuseUI)
+Vue.use(_)
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
