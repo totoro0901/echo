2df8fb0a0bd416a3cd8bb394a9167e24e52a621d
王卓 <wangzhuo@haomo-studio.com>
2018-04-27 14:59:09 +0800

    安装依赖

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入css
+import '@/assets/css/reset.css'
+
+// 第三方库
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+
 Vue.config.productionTip = false
+
+Vue.use(MuseUI)
 
 /* eslint-disable no-new */
 new Vue({
